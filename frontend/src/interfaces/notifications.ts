7e7dfa27d98f4bac14b839daebc0bc50c43de3ea
freyamade <frybrdrck@gmail.com>
2022-01-27 15:19:08 +0000

    Add notifications interface to User interface

---
+++ frontend/src/interfaces/notifications.ts
@@ -0,0 +1,7 @@
+export default interface Notifications {
+  loot_tracker_update: boolean
+  team_join: boolean
+  team_lead: boolean
+  verify_fail: boolean
+  verify_success: boolean
+}
--- frontend/src/interfaces/user.ts
+++ frontend/src/interfaces/user.ts
@@ -1,6 +1,9 @@
+import Notifications from './notifications'
+
 export default interface User {
   avatar_url: string,
   id: number | null,
+  notifications: Notifications,
   theme: string,
   username: string,
 }
