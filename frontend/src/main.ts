f192a8665c3d6bd8d9683d844c85c3f320996d04
freyamade <frybrdrck@gmail.com>
2022-02-02 15:21:07 +0000

    Added sentry to frontend code as well

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
+import * as Sentry from '@sentry/vue'
+import Vue from 'vue'
 import Notifications from 'vue-notification'
 import VModal from 'vue-js-modal'
-import Vue from 'vue'
 import VueCookies from 'vue-cookies'
 import App from './App.vue'
 import router from './router'
@@ -21,6 +22,12 @@ const dynamicDefaults = {
 Vue.use(Notifications)
 Vue.use(VModal, { dynamicDefaults })
 Vue.use(VueCookies)
+
+Sentry.init({
+  Vue,
+  dsn: 'https://06f41b525a40497a848fb726f6d03244@o242258.ingest.sentry.io/6180221',
+  logErrors: true,
+})
 
 new Vue({
   render: (h) => h(App),
