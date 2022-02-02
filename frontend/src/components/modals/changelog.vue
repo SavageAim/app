82ef5fc00e538d71d04dade2d4ec0f294222b38a
freyamade <frybrdrck@gmail.com>
2022-02-02 15:04:27 +0000

    Update changelog for 0.2.1

--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -12,6 +12,18 @@
     </div>
     <div class="card-content content">
       <h2 class="has-text-primary subtitle">{{ version }}</h2>
+      <div class="divider"><i class="material-icons icon">expand_more</i> A Message <i class="material-icons icon">expand_more</i></div>
+      <p>I had a little look at some DB metrics last night and I see a lot more people than I would have expected, since I've been slowly rolling the site out to friends' statics and such..</p>
+      <p>Needless to say, I'm sorta overwhelmed with the use (in a good way, don't worry!)</p>
+      <p>But, to those of you who have stumbled across my little website, I had no way of getting feedback from you so far...</p>
+      <p>So there's a Discord server available now! It's smol and not very good but I figured it was important to get one out at least, and we can grow from there!</p>
+      <p>Thank you to everyone who's been using the site so far, I will try my best to keep it going towards something ideal o7</p>
+      <p class="has-text-centered">
+        <a href="https://discord.gg/k8szJ5qAKw" class="button is-blurple icon-text" target="_blank">
+          <span class="icon"><img src="/discord.svg" alt="Discord Logo" /></span>
+          <span>Savage Aim Discord</span>
+        </a>
+      </p>
       <div class="divider"><i class="material-icons icon">expand_more</i> Major Changes <i class="material-icons icon">expand_more</i></div>
       <p>Notifications System has been added!</p>
       <p>
@@ -27,11 +39,8 @@
       <p>Currently the Notification system is polled every 30 seconds for updates, but I intend to make this truly live in the near future with instant updates when necessary!</p>
 
       <div class="divider"><i class="material-icons icon">expand_more</i> Minor Changes <i class="material-icons icon">expand_more</i></div>
-      <p>Added Trans Pride colour scheme.</p>
-      <p>Fixed issues causing too much side padding on some pages, especially on mobile views.</p>
-      <p>Fixed breadcrumbs for Edit and New BIS pages linking to the wrong URL.</p>
-      <p>Removed mentions of "Raid Lead", only using "Team Lead" to avoid any confusion.</p>
-      <p>Add Badges to Character cards on the Loot Tracker pages to indicate how much Loot they have already received in this Tier.</p>
+      <p>Added Traffic Lights style colour scheme.</p>
+      <p>Added more visibility into client side errors.</p>
     </div>
   </div>
 </template>
