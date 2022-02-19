ae8a76cb9cb1f9ba4f7302e77fef21c6ab1034b2
freyamade <frybrdrck@gmail.com>
2022-02-19 12:43:29 +0000

    Actually have the proper check for loot path in sentry sampler

--- backend/backend/settings_live.py
+++ backend/backend/settings_live.py
@@ -146,9 +146,13 @@ def sampler(context):
     # Always inherit parent context
     if context['parent_sampled'] is not None:
         return context['parent_sampled']
+    path = context.get('wsgi_environ', {}).get('PATH_INFO', '').lower()
     # I want to trace loot requests since I need to know what's going on in the loot requests so I can improve them
-    if 'loot' in context['transaction_context']['name'].lower():
+    if 'loot' in path:
         return 0.5
+    else if 'notifications' in path:
+        # Notifications spans are not important
+        return 0
     # Anything else I'm not too bothered by
     return 0.05
 
