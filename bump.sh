0716d919074bfc56816590778fbcf19c26e47210
freyamade <frybrdrck@gmail.com>
2022-03-10 12:42:58 +0000

    Add bump script now that version is in two places

---
+++ bump.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Function to handle getting the bump data
+get_bump() {
+    read -n 1 -p "Version to bump; [m]ajor | m[i]nor | [p]atch: " TO_BUMP
+    if [ "$TO_BUMP" = "p" ] || [ "$TO_BUMP" = "P" ]; then
+        PATCH=$((PATCH + 1))
+    elif [ "$TO_BUMP" = "i" ] || [ "$TO_BUMP" = "I" ]; then
+        MINOR=$(($MINOR + 1))
+        PATCH=0
+    elif [ "$TO_BUMP" = "m" ] || [ "$TO_BUMP" = "M" ]; then
+        MAJOR=$(($MAJOR + 1))
+        MINOR=0
+        PATCH=0
+    else
+        printf "\nIvalid choice. Try again or Ctrl+C to quit.\n"
+        get_bump
+    fi
+}
+
+# Function to handle doing the bump details
+do_bump() {
+    # Just use sed to replace the current version with the new version in shards.yml and bot_of_heart.cr
+    sed -i "s/$CURRENT_VERSION/$NEW_VERSION/" frontend/.env backend/backend/settings_live.py
+}
+
+
+CURRENT_VERSION=`cat frontend/.env | sed s/VUE_APP_VERSION=// | sed s/\"//g`
+echo -e "Current Version: \e[33m$CURRENT_VERSION\e[0m"
+
+# Split up the current version into major, minor and patch
+VERSION_LIST=(`echo $CURRENT_VERSION | tr '.' ' '`)
+MAJOR=${VERSION_LIST[0]}
+MINOR=${VERSION_LIST[1]}
+PATCH=${VERSION_LIST[2]}
+
+# Handle the bumping
+get_bump
+
+# Set the new version
+NEW_VERSION="$MAJOR.$MINOR.$PATCH"
+printf "\n"
+echo -e "New Version: \e[33m$NEW_VERSION\e[0m"
+read -n 1 -p "Is this correct? [y]: " CORRECT
+printf "\n"
+if [ "$CORRECT" = "" ] || [ "$CORRECT" = "y" ] || [ "$CORRECT" = "Y" ]; then
+    do_bump
+    exit 0
+fi
+exit 1
