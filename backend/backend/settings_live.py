b08c506ad47e5d98c253a7a9401517a30f53ac0e
freyamade <frybrdrck@gmail.com>
2022-03-10 12:36:43 +0000

    Add release data to sentry

--- backend/backend/settings_live.py
+++ backend/backend/settings_live.py
@@ -175,5 +175,6 @@ sentry_sdk.init(
 
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
+    send_default_pii=True,
+    release='savageaim@0.4.3',
 )
