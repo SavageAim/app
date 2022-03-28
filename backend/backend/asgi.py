b23e7e182aa054e4964497c31a3af56acabe344e
freyamade <frybrdrck@gmail.com>
2022-03-28 13:35:20 +0100

    Created an asgi application file for deploying websockets

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from api.routing import ws_routes
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(URLRouter(ws_routes)),
+})
