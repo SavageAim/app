2ad6d1c68ba53c9268f5effe91730061859c6109
freyamade <frybrdrck@gmail.com>
2022-04-07 15:54:16 +0100

    Fixed new bump script

--- bump.sh
+++ bump.sh
@@ -2,7 +2,7 @@
 
 # Function to handle doing the bump details
 do_bump() {
-    # Just use sed to replace the current version with the new version in shards.yml and bot_of_heart.cr
+    # Just use sed to replace the current version with the new one in the files that need it
     sed -i "s/$CURRENT_VERSION/$NEW_VERSION/" frontend/.env backend/backend/settings_live.py
 }
 
@@ -11,7 +11,7 @@ CURRENT_VERSION=`cat frontend/.env | sed s/VUE_APP_VERSION=// | sed s/\"//g`
 echo -e "Current Version: \e[33m$CURRENT_VERSION\e[0m"
 
 # Replace the current version with the current date
-printf -v NEW_VERSION '%(%Y-%m-%d)T\n' -1
+printf -v NEW_VERSION '%(%Y-%m-%d)T' -1
 printf "\n"
 echo -e "New Version: \e[33m$NEW_VERSION\e[0m"
 read -n 1 -p "Is this correct? [Y/n]: " CORRECT
