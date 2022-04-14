06c37a9ea97a36d32218b1ff9fb08543ea1add0d
freyamade <frybrdrck@gmail.com>
2022-04-14 11:12:37 +0100

    add management command to update existing worlds

---
+++ backend/api/management/commands/fix_char_worlds.py
@@ -0,0 +1,18 @@
+from datetime import timezone
+from django.core.management.base import BaseCommand
+from api.models import Character, Notification
+from api.serializers.character import NEW_WORLD_PATTERN
+
+
+class Command(BaseCommand):
+    help = 'Fix any characters that have been imported while the world names are broken'
+
+    def handle(self, *args, **options):
+        for obj in Character.objects.all():
+            find = NEW_WORLD_PATTERN.findall(obj.world)
+            if len(find) == 1:
+                self.stdout.write(f'Found bad world pattern - {obj.world}')
+                world = f'{find[0][0]} ({find[0][1]})'
+                self.stdout.write(f'Replaced with - {world}')
+                obj.world = world
+                obj.save()
