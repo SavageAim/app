bbb76b60da0b47d55711f7b9c66f1373215c59c1
freyamade <frybrdrck@gmail.com>
2022-04-14 11:14:50 +0100

    bump

--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -11,35 +11,10 @@
       </div>
     </div>
     <div class="card-content content">
-      <h2 class="has-text-primary subtitle">{{ version }} - The First Major Release!</h2>
-      <div class="divider"><i class="material-icons icon">expand_more</i> Major Changes <i class="material-icons icon">expand_more</i></div>
-      <p>
-        Websocket functionality has been added to the system, causing all updates to be received immediately without need of refreshing the page.
-        <ul>
-          <li>Any page that can be updated by a source other than the current user has this feature added, allowing for immediate feedback of other user's actions.</li>
-          <li class="has-text-danger">Please note this was difficult to test and some issues may occur. Please report any issues in the Discord as best you can, thank you!</li>
-        </ul>
-      </p>
-      <p>
-        Also, the version scheme has changed going forward. Instead of SemVer the site will use dates of release!
-        <ul>
-          <li>More info <a href="https://wiki.savageaim.com/developer-visibility/changelog" target="_blank">here</a>.</li>
-        </ul>
-      </p>
-
-      <div class="divider"><i class="material-icons icon">expand_more</i> FFXIV 6.1 <i class="material-icons icon">expand_more</i></div>
-      <p>
-        Added the following Gear with the release of 6.1;
-        <ul>
-          <li>Bluefeather; Item Level 595 Weapons</li>
-          <li>Augmented Classical; Item Level 590 Weapons, Armour, Accessories</li>
-          <li>Panthean; Item Level 590 Armour</li>
-        </ul>
-      </p>
-
+      <h2 class="has-text-primary subtitle">{{ version }}</h2>
       <div class="divider"><i class="material-icons icon">expand_more</i> Minor Changes <i class="material-icons icon">expand_more</i></div>
-      <p>Added the External URL links to the footer of the cards in the BIS section of the Character page.</p>
-      <p>Added a Notification for when Teams get renamed.</p>
+      <p>Fixes for a couple of issues found in the websocket deployment.</p>
+      <p>Fix for issue with characters being pulled from Lodestone with a broken world name string.</p>
     </div>
   </div>
 </template>
