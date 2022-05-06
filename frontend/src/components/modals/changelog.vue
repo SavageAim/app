2309d0e076c4c824dbd48f15ecb361013d17d780
freyamade <frybrdrck@gmail.com>
2022-05-06 12:07:27 +0100

    Put alias update into the major section

--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -12,13 +12,14 @@
     </div>
     <div class="card-content content">
       <h2 class="has-text-primary subtitle">{{ version }}</h2>
-      <div class="divider"><i class="material-icons icon">expand_more</i> Minor Changes <i class="material-icons icon">expand_more</i></div>
+      <div class="divider"><i class="material-icons icon">expand_more</i> Major Changes <i class="material-icons icon">expand_more</i></div>
       <p>
         Added a field for Characters to have aliases, alternative names they can go by.
         <ul>
           <li>This is accessible in the Character page, under the View Settings tab!</li>
         </ul>
       </p>
+      <div class="divider"><i class="material-icons icon">expand_more</i> Minor Changes <i class="material-icons icon">expand_more</i></div>
       <p>Fixing performance issues found by some online performance checking tools.</p>
       <p>Preventing random navigation errors in vue router code.</p>
     </div>
