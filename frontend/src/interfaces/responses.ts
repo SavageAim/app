8f43f904ec29ffef60b1eb0b1509433a4342d861
freyamade <frybrdrck@gmail.com>
2022-06-13 10:48:39 +0100

    fixing interface build issue that I missed

--- frontend/src/interfaces/responses.ts
+++ frontend/src/interfaces/responses.ts
@@ -44,7 +44,7 @@ export interface CreateResponse {
 
 // Interface defining the potential error responses that can come from the Character.create method
 export interface CharacterCreateErrors {
-  lodestone_id?: string[]
+  lodestone_id: string[]
   [key: string]: string[]
 }
 
