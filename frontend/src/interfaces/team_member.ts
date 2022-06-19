cfd63543d4d87336e2fef4085edcffd9995ab97c
freyamade <frybrdrck@gmail.com>
2022-06-19 22:16:00 +0100

    Updated teammember interface in frontend to include permissions

--- frontend/src/interfaces/team_member.ts
+++ frontend/src/interfaces/team_member.ts
@@ -1,10 +1,16 @@
 import BISList from './bis_list'
 import { Character } from './character'
 
+interface Permissions {
+  loot_manager: boolean
+  team_characters: boolean
+}
+
 export default interface TeamMember {
   bis_list: BISList
   character: Character
   id: number
   lead: boolean
   name: string
+  permissions: Permissions
 }
