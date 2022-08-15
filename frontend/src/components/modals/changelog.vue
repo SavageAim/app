dc926f6b870ae7527b3f9145541899344dd55424
freyamade <frybrdrck@gmail.com>
2022-08-15 19:05:45 +0100

    Revert "update changelog"
    This reverts commit 9e0a46931ff3c782c8eb9a95d0c2b470ae938902.

--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -62,7 +62,6 @@
         </ul>
       </p>
       <p>Fixed issue where certain parts of the Edit Proxy page were not displaying information correctly.</p>
-      <p>Team Join page now has special handling for when the user has no Characters yet.</p>
 
     </div>
   </div>
