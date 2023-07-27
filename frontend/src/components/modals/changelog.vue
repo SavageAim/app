e4df459c6563c32e1d225ad8517a8a777a30df43
freyamade <frybrdrck@gmail.com>
2023-07-27 17:44:32 +0100

    uploading current wip changelog

--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -13,11 +13,16 @@
     <div class="card-content content">
       <h2 class="has-text-primary subtitle">{{ version }}</h2>
 
-      <div class="divider"><i class="material-icons icon">expand_more</i> FFXIV Patch 6.45 <i class="material-icons icon">expand_more</i></div>
-      <p>Added Majestic Manderville Weapons, iL 645.</p>
+      <div class="divider"><i class="material-icons icon">expand_more</i> Settings Page Cleanup <i class="material-icons icon">expand_more</i></div>
+      <p>The <router-link to="/settings/">settings page</router-link> has been expanded with extra settings.</p>
+      <p>In the interest of keeping it neat, the settings sections are now accessible in separate sections, similar to the character pages.</p>
+      <p>Navbar items will display an asterisk when they have been changed, to help indicate unsaved changes have been made.</p>
 
-      <div class="divider"><i class="material-icons icon">expand_more</i> Fixes <i class="material-icons icon">expand_more</i></div>
-      <p>Rewrote Etro Import endpoint to prevent rate limit hits. Had the unplanned effect of speeding up the request as well.</p>
+      <div class="divider"><i class="material-icons icon">expand_more</i> Loot Manager Version 2 <i class="material-icons icon">expand_more</i></div>
+      <p>A new version of the Loot Manager has been released in beta.</p>
+      <p>To view the differences, or opt in to try it out, please visit <router-link to="/settings/">your settings</router-link>.</p>
+      <p class="has-text-primary">If you have any feedback, please make it known in the <a href="https://discord.gg/k8szJ5qAKw" target="_blank">Discord server</a>!</p>
+      <p>Also note that the Loot Manager version is a personal setting, as both will be supported going forward.</p>
 
     </div>
   </div>
