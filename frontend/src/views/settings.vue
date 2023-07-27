72619771fd090894c75cd878e71e2ae0a349338b
freyamade <frybrdrck@gmail.com>
2023-07-27 15:00:21 +0100

    divided the settings page into tabs

--- frontend/src/views/settings.vue
+++ frontend/src/views/settings.vue
@@ -1,33 +1,59 @@
 <template>
   <div>
-    <h2 class="title">
-      Settings for {{ user.username }}
-    </h2>
-
-    <!-- Colour Scheme -->
+    <!-- Navigation -->
     <div class="columns">
-      <div class="column is-half">
+      <div class="column is-one-quarter-desktop">
+        <div class="card">
+          <div class="card-header">
+            <div class="card-header-title">Settings for {{ user.username }}</div>
+          </div>
+          <div class="card-content">
+            <aside class="menu">
+              <ul class="menu-list">
+                <li>
+                  <a :class="{ 'is-active': activeTab.theme }" @click="showTheme">
+                    Colour Scheme <span v-if="unsavedTheme()">*</span>
+                  </a>
+                </li>
+                <li>
+                  <a :class="{ 'is-active': activeTab.notifications }" @click="showNotifications">
+                    Notifications <span v-if="unsavedNotifications()">*</span>
+                  </a>
+                </li>
+                <li>
+                  <a :class="{ 'is-active': activeTab.lootManager }" @click="showLootManager">
+                    Loot Manager Version <span v-if="unsavedLootManager()">*</span>
+                  </a>
+                </li>
+              </ul>
+            </aside>
+          </div>
+        </div>
+        <button class="button is-success is-fullwidth" @click="save">Save</button>
+      </div>
+
+      <!-- Notifications -->
+      <div class="column">
+        <!-- Colour Scheme -->
         <ThemeSettings
           :errors="errors"
           :theme="theme"
           v-on:changeTheme="changeTheme"
+          v-if="activeTab.theme"
         />
-      </div>
-
-      <!-- Notifications -->
-      <div class="column is-half">
         <NotificationsSettings
           :errors="errors"
           :notifications="notifications"
           v-on:changeNotification="changeNotification"
+          v-if="activeTab.notifications"
         />
       </div>
     </div>
-    <button class="button is-success is-fullwidth" @click="save">Save</button>
   </div>
 </template>
 
 <script lang="ts">
+import isEqual from 'lodash.isequal'
 import { Component, Watch } from 'vue-property-decorator'
 import NotificationsSettings from '@/components/settings/notifications.vue'
 import ThemeSettings from '@/components/settings/theme.vue'
@@ -43,6 +69,12 @@ import SavageAimMixin from '@/mixins/savage_aim_mixin'
   },
 })
 export default class Settings extends SavageAimMixin {
+  activeTab = {
+    theme: true,
+    notifications: false,
+    lootManager: false,
+  }
+
   errors: SettingsErrors = {}
 
   notifications = {
@@ -85,6 +117,12 @@ export default class Settings extends SavageAimMixin {
     this.notifications = { ...this.$store.state.user.notifications }
   }
 
+  resetActiveTab(): void {
+    this.activeTab.theme = false
+    this.activeTab.notifications = false
+    this.activeTab.lootManager = false
+  }
+
   // Save the data into a new bis list
   async save(): Promise<void> {
     const body = JSON.stringify({ theme: this.theme, notifications: this.notifications })
@@ -112,6 +150,34 @@ export default class Settings extends SavageAimMixin {
     catch (e) {
       this.$notify({ text: `Error ${e} when attempting to update User Settings.`, type: 'is-danger' })
     }
+  }
+
+  showLootManager(): void {
+    this.resetActiveTab()
+    this.activeTab.lootManager = true
+  }
+
+  showNotifications(): void {
+    this.resetActiveTab()
+    this.activeTab.notifications = true
+  }
+
+  showTheme(): void {
+    this.resetActiveTab()
+    this.activeTab.theme = true
+  }
+
+  unsavedLootManager(): boolean {
+    // TODO
+    return false
+  }
+
+  unsavedNotifications(): boolean {
+    return !isEqual(this.notifications, this.user.notifications)
+  }
+
+  unsavedTheme(): boolean {
+    return this.theme !== this.user.theme
   }
 }
 </script>
