1fcce7ba8955918a71faef7c33f9f1ecb8efa346
freyamade <frybrdrck@gmail.com>
2023-09-30 16:38:55 +0100

    add backend update info to changelog

--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -18,7 +18,17 @@
       <p>In the interest of keeping it neat, the settings sections are now accessible in separate sections, similar to the character pages.</p>
       <p>Navbar items will display an asterisk when they have been changed, to help indicate unsaved changes have been made.</p>
 
-      <div class="divider"><i class="material-icons icon">expand_more</i> Loot Manager Version 2 <i class="material-icons icon">expand_more</i></div>
+      <div class="divider"><i class="material-icons icon">expand_more</i> Loot Manager Update <i class="material-icons icon">expand_more</i></div>
+      <p>
+        The "Mount", "Tome Weapon Token" and "Tome Weapon Augment" fields in the loot manager are now returned by the API.
+        <ul>
+          <li>They have been given naive implementation; if a team member has not yet received one, they will appear in the need list.</li>
+          <li>Also, for both fields involving Tome Weapons, every member will appear in greed.</li>
+          <li>Mount will never have anyone in the greed field for it as it is impossible to get a mount twice.</li>
+        </ul>
+      </p>
+
+      <div class="divider"><i class="material-icons icon">expand_more</i> Per Fight Loot Manager <i class="material-icons icon">expand_more</i></div>
       <p>A new version of the Loot Manager has been released in beta.</p>
       <p>To view the differences, or opt in to try it out, please visit <router-link to="/settings/">your settings</router-link>.</p>
       <p class="has-text-primary">If you have any feedback, please make it known in the <a href="https://discord.gg/k8szJ5qAKw" target="_blank">Discord server</a>!</p>
