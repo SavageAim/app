dbdd1bdfcced8289a1ba92488b9d6ac677ea77b7
freyamade <frybrdrck@gmail.com>
2024-05-19 02:23:54 +0200

    add info about the new loot solver, along with a link to the new wiki page

--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -12,13 +12,19 @@
     </div>
     <div class="card-content content">
       <h2 class="has-text-primary subtitle">{{ version }}</h2>
-      <div class="divider"><i class="material-icons icon">expand_more</i> New Features <i class="material-icons icon">expand_more</i></div>
+      <div class="divider"><i class="material-icons icon">expand_more</i> Import from Lodestone <i class="material-icons icon">expand_more</i></div>
       <p>
         It is now possible to import your current gear from Lodestone when creating/editing BIS Lists!
         <ul>
           <li>If your current gear on Lodestone is not equippable by the Job your BIS List is set to, it will give a message and not make any changes.</li>
         </ul>
       </p>
+
+      <div class="divider"><i class="material-icons icon">expand_more</i> Loot Manager - Solver <i class="material-icons icon">expand_more</i></div>
+      <p>A new feature has been added to the Loot Manager page; the Loot Solver.</p>
+      <p>This feature aims to "solve" loot distibution by handing out loot fairly but in a way that ensures you spend the least number of weeks on each fight as possible.</p>
+      <p>It is accessible in a dropdown box on the Loot Manager page, and Members with the required permission can use it to auto-fill the loot for each fight each week.</p>
+      <p class="has-text-info">For more details, please see <a href="https://wiki.savageaim.com/teams/loot-manager/loot-solver" target="_blank">the wiki page</a> about this feature!</p>
 
       <div class="divider"><i class="material-icons icon">expand_more</i> Continuing UI Consistency <i class="material-icons icon">expand_more</i></div>
       <p>Colours of Action buttons in BIS List pages have been changed to a darker blue to free-up the lighter blue for specific purposes.</p>
