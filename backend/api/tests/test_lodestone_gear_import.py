dd9bfa039386652e3e0ed6230b897a161277c20a
freyamade <frybrdrck@gmail.com>
2024-05-25 19:04:03 +0200

    fix lodestone tests

--- backend/api/tests/test_lodestone_gear_import.py
+++ backend/api/tests/test_lodestone_gear_import.py
@@ -28,7 +28,7 @@ class LodestoneGearImport(SavageAimTestCase):
         Test Plan;
             - Import Eira and ensure her gear matches what I currently had equipped
         """
-        url = reverse('api:lodestone_gear_import', kwargs={'character_id': '22909725', 'expected_job': 'BLM'})
+        url = reverse('api:lodestone_gear_import', kwargs={'character_id': '22909725', 'expected_job': 'PLD'})
         user = self._get_user()
         self.client.force_authenticate(user)
         response = self.client.get(url)
@@ -36,21 +36,21 @@ class LodestoneGearImport(SavageAimTestCase):
 
         # Build an expected data packet
         expected = {
-            'job_id': 'BLM',
+            'job_id': 'PLD',
             'mainhand': Gear.objects.get(name='Voidcast').pk,
             'offhand': Gear.objects.get(name='Voidcast').pk,
-            'head': Gear.objects.get(name='Diadochos', has_armour=True).pk,
-            'body': Gear.objects.get(name='Diadochos', has_armour=True).pk,
+            'head': Gear.objects.get(name='Ascension', has_armour=True).pk,
+            'body': Gear.objects.get(name='Augmented Credendum', has_armour=True).pk,
             'hands': Gear.objects.get(name='Diadochos', has_armour=True).pk,
-            'legs': Gear.objects.get(name='Diadochos', has_armour=True).pk,
-            'feet': Gear.objects.get(name='Diadochos', has_armour=True).pk,
-            'earrings': Gear.objects.get(name='Diadochos', has_accessories=True).pk,
-            'necklace': Gear.objects.get(name='Diadochos', has_accessories=True).pk,
+            'legs': Gear.objects.get(name='Ascension', has_armour=True).pk,
+            'feet': Gear.objects.get(name='Augmented Credendum', has_armour=True).pk,
+            'earrings': Gear.objects.get(name='Augmented Credendum', has_accessories=True).pk,
+            'necklace': Gear.objects.get(name='Ascension', has_accessories=True).pk,
             'bracelet': Gear.objects.get(name='Diadochos', has_accessories=True).pk,
-            'right_ring': Gear.objects.get(name='Diadochos', has_accessories=True).pk,
-            'left_ring': Gear.objects.get(name='Diadochos', has_accessories=True).pk,
+            'right_ring': Gear.objects.get(name='Credendum', has_accessories=True).pk,
+            'left_ring': Gear.objects.get(name='Ascension', has_accessories=True).pk,
             'min_il': 640,
-            'max_il': 645,
+            'max_il': 660,
         }
         self.maxDiff = None
         self.assertDictEqual(response.json(), expected)
@@ -79,5 +79,5 @@ class LodestoneGearImport(SavageAimTestCase):
         self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
         self.assertEqual(
             response.json()['message'],
-            'Error occurred when attempting to import gear. Gear was expected to be for "SAM", but "THM BLM" was found.',
+            'Error occurred when attempting to import gear. Gear was expected to be for "SAM", but "GLA PLD" was found.',
         )
