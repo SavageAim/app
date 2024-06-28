2f4bbcbd44f2a8348de2e8c2cb26ea952ad7ccbb
freyamade <frybrdrck@gmail.com>
2024-06-28 22:45:55 +0100

    fix add extra item names command

--- backend/api/management/commands/add_extra_names_to_gear.py
+++ backend/api/management/commands/add_extra_names_to_gear.py
@@ -14,18 +14,23 @@ class Command(BaseCommand):
         gear_data_dir = settings.BASE_DIR / 'api/management/commands/seed_data/gear'
 
         # Get Gear data, double check that the Gear has the required information
-        with scandir(gear_data_dir) as gear_files:
-            for file in gear_files:
-                with open(file, 'r') as f:
-                    data = yaml.safe_load(f)
-                    for item in data:
-                        obj = models.Gear.objects.get(name=item['name'], item_level=item['item_level'])
-                        changed = False
-                        if len(obj.extra_import_names) == 0 and len(item.get('extra_import_names', [])) > 0:
-                            changed = True
-                            obj.extra_import_names = item['extra_import_names']
-                        if len(obj.extra_import_classes) == 0 and len(item.get('extra_import_classes', [])) > 0:
-                            changed = True
-                            obj.extra_import_classes = item['extra_import_classes']
-                        if changed:
-                            obj.save()
+        with scandir(gear_data_dir) as expac_dirs:
+            for expac_dir in expac_dirs:
+                if not expac_dir.is_dir():
+                    continue
+
+                with scandir(expac_dir.path) as gear_files:
+                    for file in gear_files:
+                        with open(file, 'r') as f:
+                            data = yaml.safe_load(f)
+                            for item in data:
+                                obj = models.Gear.objects.get(name=item['name'], item_level=item['item_level'])
+                                changed = False
+                                if len(obj.extra_import_names) == 0 and len(item.get('extra_import_names', [])) > 0:
+                                    changed = True
+                                    obj.extra_import_names = item['extra_import_names']
+                                if len(obj.extra_import_classes) == 0 and len(item.get('extra_import_classes', [])) > 0:
+                                    changed = True
+                                    obj.extra_import_classes = item['extra_import_classes']
+                                if changed:
+                                    obj.save()
