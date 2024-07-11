78dd57131ef6fac1f927d474800392ba4f087714
freyamade <freya.broderick@honu.ai>
2024-07-11 17:12:17 +0200

    add documentation to job views

--- backend/api/views/job.py
+++ backend/api/views/job.py
@@ -18,7 +18,8 @@ class JobCollection(APIView):
     """
     Get a list of Jobs in the system
     """
-
+    queryset = Job
+    serializer_class = JobSerializer(many=True)
     permission_classes = [AllowAny]
 
     def get(self, request: Request) -> Response:
@@ -35,12 +36,16 @@ class JobSolverSortCollection(APIView):
     """
     Get a list of Jobs in the system, ordered by the default order of how jobs are sorted in the solver by default
     """
-
+    queryset = Job
+    serializer_class = JobSerializer(many=True)
     permission_classes = [AllowAny]
 
     def get(self, request: Request) -> Response:
         """
-        Return the full list of Jobs in default ordering (Melee > Ranged > Caster > Tanks > Healer)
+        Return the full list of Jobs in Loot Solver default order (Melee > Ranged > Caster > Tank > Healer)
+
+        The point of this endpoint is to allow the TeamSettings page to display the ordering of Jobs within the Loot Solver sorting.
+        Teams only store overrides, so this view is available to retrieve the default, and then overrides are moved around as needed.
         """
         # Permissions won't allow this method to be run by non-auth'd users
         objs = Job.get_in_solver_order()
