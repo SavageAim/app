e23eaac58a91dcfcc92ebe0a6d250dfec3180690
freyamade <frybrdrck@gmail.com>
2024-07-12 20:14:25 +0200

    added a client for running esql queries against xivapi

---
+++ backend/api/tests/test_xivapi_search_client.py
@@ -0,0 +1,18 @@
+from .test_base import SavageAimTestCase
+from ..xivapi_item_search_client import XIVAPIItemSearcher
+
+
+class XIVAPISearchTestSuit(SavageAimTestCase):
+    """
+    Attempt to search the XIVAPI for a set of items, and ensure the correct information is returned.
+    """
+
+    def test_search(self):
+        """
+        Attempt to search the XIVAPI for a set of items, and ensure the correct information is returned.
+        """
+        expected = {
+            42722: {'name': 'Neo Kingdom Thighboots of Fending', 'item_level': 700},
+            42788: {'name': 'Skyruin Gunblade', 'item_level': 710},
+        }
+        self.assertDictEqual(expected, XIVAPIItemSearcher.get_item_information(*expected.keys()))
