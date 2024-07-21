cdad4d9305ed160fd359985e8feee03609fd051d
freyamade <frybrdrck@gmail.com>
2024-07-22 01:19:45 +0200

    moving to xivapi beta

--- backend/api/views/xivgear.py
+++ backend/api/views/xivgear.py
@@ -149,8 +149,7 @@ class XIVGearImport(ImportAPIView):
             gear_names = XIVAPISearchClient.get_item_information(*sa_gear.values())
         except requests.HTTPError as e:
             return Response({'message': e.response.text}, 400)
-
-        if len(gear_names) != len(sa_gear):
+        except ValueError:
             return Response({'message': 'Could not find some of the items on XIVAPI, please try again later!'}, 400)
 
         # Use the returned map to calculate the min and max ils, and also replace IDs with names in sa_gear
