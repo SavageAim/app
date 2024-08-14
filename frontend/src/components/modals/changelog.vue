e3df1b9ca91b0dfcf1695b24a61ef0974fa71cb1
freyamade <freya.broderick@honu.ai>
2024-08-14 10:40:28 +0200

    Fixed an issue that was happening when trying to Auto-Assign Loot from the Solver when there were Greed slots for the week

--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -12,10 +12,8 @@
     </div>
     <div class="card-content content">
       <h2 class="has-text-primary subtitle">{{ version }}</h2>
-      <div class="divider"><i class="material-icons icon">expand_more</i> Loot Solver Update <i class="material-icons icon">expand_more</i></div>
-      <p>Discovered a major issue with the Loot Solver algorithm being inefficient due to it being super naive.</p>
-      <p>This release improves the algorithm to be more true to the "top N people get items every week".</p>
-      <p>A whole writeup of the methodology is available <a href="https://github.com/orgs/SavageAim/discussions/1" target="_blank">here</a>, in case anyone wants to have a read!</p>
+      <div class="divider"><i class="material-icons icon">expand_more</i> Fixes and Improvements <i class="material-icons icon">expand_more</i></div>
+      <p>Fixed an issue that was happening when trying to Auto-Assign Loot from the Solver when there were Greed slots for the week.</p>
     </div>
   </div>
 </template>
