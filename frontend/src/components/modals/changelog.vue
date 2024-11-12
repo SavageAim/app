7cb55d1e737f1b1c479dc89d926d639b2a5f8487
freyamade <freya.broderick@honu.ai>
2024-11-12 11:21:19 +0100

    update changelog

--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -12,6 +12,25 @@
     </div>
     <div class="card-content content">
       <h2 class="has-text-primary subtitle">{{ version }}</h2>
+      <div class="divider"><i class="material-icons icon">expand_more</i> FFXIV 7.1 Update <i class="material-icons icon">expand_more</i></div>
+      <p>
+        Added the following new types of Gear released in 7.1;
+        <ul>
+          <li>Augmented Archeo Kingdom Weapons, Armour, and Accessories - Item Level 720</li>
+          <li>Ark Angel's Armour - Item Level 720</li>
+          <li>New EX Trial Weapons - Item Level 725</li>
+        </ul>
+      </p>
+
+      <div class="divider"><i class="material-icons icon">expand_more</i> Quick-Set to BIS <i class="material-icons icon">expand_more</i></div>
+      <p>Thank you Rak for the feature request!</p>
+      <p>
+        Now, whenever the Current Gear for a slot does not match the slot's BIS Gear, a button will appear to the right of the dropdown to allow you to instantly set the Current value to the BIS value.
+        <ul>
+          <li>This is in the hopes of making life a little easier for those times where you buy your BIS item, for example!</li>
+        </ul>
+      </p>
+
       <div class="divider"><i class="material-icons icon">expand_more</i> Bugfixes <i class="material-icons icon">expand_more</i></div>
       <p>
         Fixed a timezone-based bug when creating manual Loot Entries.
@@ -21,13 +40,10 @@
         </ul>
       </p>
       <p>Fixed an issue where the mobile version of the Loot Solver would show incorrect numbers for some fields.</p>
+
+      <div class="divider"><i class="material-icons icon">expand_more</i> Minor Updates <i class="material-icons icon">expand_more</i></div>
       <p>Improved the Viper and Pictomancer icons finally!</p>
       <p>Fixed some issues in the OpenAPI schema!</p>
-
-      <div class="divider"><i class="material-icons icon">expand_more</i> Quick-Set to BIS <i class="material-icons icon">expand_more</i></div>
-      <p>Thank you Rak for the feature request!</p>
-      <p>All of the Current Gear dropdowns on the Edit BIS pages now have a button you can click to instantly set the Current value to your set BIS value for that slot!</p>
-      <p>This just brings a little bit of QoL to anyone updating their BIS Lists manually, after purchasing items for example!</p>
     </div>
   </div>
 </template>
