297b0273be9e6c2a973172cf7714c47eea91a9da
freyamade <freya.broderick@honu.ai>
2024-11-26 12:39:29 +0100

    bump

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -30,7 +30,7 @@ Sentry.init({
   Vue,
   dsn: 'https://06f41b525a40497a848fb726f6d03244@o242258.ingest.sentry.io/6180221',
   logErrors: true,
-  release: 'savageaim@20241112',
+  release: 'savageaim@20241126',
   integrations: [
     new Sentry.BrowserTracing({
       routingInstrumentation: Sentry.vueRouterInstrumentation(router),
