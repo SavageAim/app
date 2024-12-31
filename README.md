9b9989f964b8293f91578cec24237922911e3f9a
freyamade <frybrdrck@gmail.com>
2024-12-31 18:30:27 +0100

    add developer setup to readme and a management command for dev setup has been written

--- README.md
+++ README.md
@@ -7,39 +7,68 @@
 <h4 align="center">A Best-in-Slot manager for you and your Final Fantasy XIV raid team!</h4>
 
 <p align="center">
-  <img src="https://img.shields.io/github/v/release/SavageAim/app" alt="Latest Release" />
+  <a href="https://github.com/SavageAim/app/releases/latest" target="_blank">
+    <img src="https://img.shields.io/github/v/release/SavageAim/app" alt="Latest Release" />
+  </a>
   <a href="https://wiki.savageaim.com" target="_blank">
     <img alt="Wiki" src="https://img.shields.io/badge/GitBook-wiki-blue?logo=gitbook">
   </a>
 </p>
 
-<p align="center">
-  <a href="#key-features">Key Features</a> •
-  <a href="#how-to-use">How To Use</a> •
-  <a href="#download">Development</a> •
-  <a href="#related">Related</a> •
-  <a href="#license">License</a>
-</p>
-
 ## Key Features
-- Track your own Best-in-Slot lists for multiple Jobs.
-- Create Teams to see your entire raid team's current loot progression status.
-- Access Loot Tracking systems that automatically update your Team Member's BIS information when they recieve items.
-- Also access a (beta) Solver for optimising loot handouts in the fairest way to get you finished with a fight ASAP.
+- Track your own Best-in-Slot lists for multiple Jobs, with available importing from Etro or XIVGear!
+- Create Teams to track the gearing requirements/progress of everyone in your raid team!
+- Access Loot Tracking systems that automatically update your Team Member's BIS information when they recieve drops!
+- Also access a solver for optimising loot handouts in the fairest way to get you finished with a fight ASAP!
 - And if your friends don't want to join the site, make proxies for all their characters to still get all of these features!
 
 ## How To Use
 Sign up using your Discord account on https://savageaim.com and go from there!
 If you have any issues or questions, feel free to join the Discord from the link on the footer of the site, or open an issue here!
 
-## Development
-TODO - Will write this section once I set up my new laptop and figure out the hoops I have to jump through :D
-I expect to provide a small test DB with information on how to login to the test account for ease of development.
-
 ## Related
 [Savage Aim Dalamud Plugin](https://github.com/SavageAim/plugin) - Dalamud Plugin for updating your BIS Lists from in-game, if you use those sorts of things
 
-## License
+## Development
+Running SavageAim locally for development has a few steps but is relatively straightforward;
 
-MIT
+### Requirements
+- Docker installed, preferably a version that comes with `compose` though for older versions `docker-compose` can be installed separately.
+- Python 3.8 or higher, and pip
+- NodeJS 16.x
+
+### Instructions
+1. Clone the repo
+2. In the root directory, run `docker compose up -d` to set up the service containers for *redis*, *postgres*, and *nginx* as a load balancer
+3. Backend setup, skip these steps if you have already run the backend before
+   1. Navigate to `backend` and run `pip3 install -r requirements.txt` to install the requirements. Feel free to create a virtualenv in advance if you wish!
+   2. Run `python3 manage.py migrate` to apply migrations to the postgres container. Postgres data is saved under `database-data` in the top level directory.
+   3. Run `python3 manage.py seed` to populate the DB with all of the Gear, Tier, and other constant information.
+   3. Run `python3 manage.py dev_setup` to set up the local site for you to be able to log in and use.
+4. Run `python3 manage.py runserver 0.0.0.0:8000` to bring up a development server for the backend. Leave this terminal tab running this command to leave the dev backend running.
+5. In a separate terminal tab/window, navigate to the `frontend` directory. Run `npm install` if you haven't previously.
+6. Run `npm run serve` to bring up a development frontend.
+7. Visit `localhost:8080` in your browser to view the site locally. This port actually accesses the *nginx* container to ensure requests are routed properly between the front and backends.
+
+### Usage
+To log in and start using the site, you will need to visit `localhost:8080/backend/admin` and log in using the form on that page with username `devuser` and password `password`.
+Once the login is successful, returning to `localhost:8080` will let you use the site as normal.
+
+The devuser is set up with the following information;
+- 2 Characters;
+  - Character 1 is unverified, allowing you to see/interact with the site with an unverified character.
+  - Character 2 is verified, allowing you to see/interact with the site with a verified character.
+    - Character 2 also has 3 BIS Lists, one for each role, and has joined each Team with one of these, allowing vision of the Loot Solver Need/Greed setups for them in every Team.
+- 3 Teams;
+  - Team 1 is led by Character 2, and contains Character 3, a Character belonging to another user.
+  - Team 2 is led by Character 3, with Character 2 as a Member with no permissions.
+  - Team 3 is also led by Character 3, but Character 2 now has Loot Manager and Proxy Manager permissions.
+  - All Teams also contain Character 4, a proxy Character.
+
+If you wish to access Character 3's user to edit permissions of Character 2 in either Team, log out and then log in with `devuser2` instead to access that account.
+
+Re-running the `python3 manage.py dev_setup` command will give you a message stating that the DB was already set up, and ask if you want to reset it.
+This just means that if you change things and want a clean slate, just re-run the command and say yes.
+
+If there are any issues with local development, please let me know either on Discord or as an Issue on the repo!
 
---
+++ backend/api/management/commands/dev_setup.py
@@ -0,0 +1,292 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from api import models
+
+
+class Command(BaseCommand):
+    help = 'Populate the Dev DB with some data that should make it useable by new devs.'
+
+    def handle(self, *args, **options):
+        # Firstly, ensure we're using local by attempting to import from admin
+        if User.objects.filter(username='devuser').exists():
+            response = input('DB appears to already be set up. Would you like to recreate it? [y/N] ')
+            if len(response) == 0 or response.lower()[0] != 'y':
+                return
+            # Wipe the DB and set it all up again
+            print('Cleaning up existing objects')
+            models.Team.objects.all().delete()
+            models.BISList.objects.all().delete()
+            models.Character.objects.all().delete()
+            User.objects.all().delete()
+
+        print('Creating Users')
+        user1 = User.objects.create_superuser(username='devuser', password='password')
+        user2 = User.objects.create_superuser(username='devuser2', password='password')
+
+        print('Creating Characters')
+        char1 = models.Character.objects.create(
+            avatar_url='https://placehold.co/96/2E53A5/F3F3EC.png?text=1',
+            lodestone_id='1',
+            name='Character 1',
+            token=models.Character.generate_token(),
+            user=user1,
+            world='Dev',
+        )
+        char2 = models.Character.objects.create(
+            avatar_url='https://placehold.co/96/2E53A5/F3F3EC.png?text=2',
+            lodestone_id=2,
+            name='Character 2',
+            token=models.Character.generate_token(),
+            user=user1,
+            verified=True,
+            world='Dev',
+        )
+        char3 = models.Character.objects.create(
+            avatar_url='https://placehold.co/96/2E53A5/F3F3EC.png?text=3',
+            lodestone_id='3',
+            name='Character 3',
+            token=models.Character.generate_token(),
+            user=user2,
+            verified=True,
+            world='Dev',
+        )
+        char4 = models.Character.objects.create(
+            avatar_url='https://placehold.co/96/2E53A5/F3F3EC.png?text=4',
+            lodestone_id=4,
+            name='Character 4',
+            token=models.Character.generate_token(),
+            user=None,
+            verified=True,
+            world='Dev',
+        )
+
+        # Get the info for setting up BIS Lists and Teams
+        tier = models.Tier.objects.first()
+        raid_weapon = models.Gear.objects.get(
+            item_level=tier.max_item_level,
+            name=tier.raid_gear_name,
+            has_weapon=True,
+        )
+        raid_gear = models.Gear.objects.get(
+            item_level=tier.max_item_level-5,
+            name=tier.raid_gear_name,
+            has_weapon=False,
+        )
+        tome_gear = models.Gear.objects.get(
+            item_level=tier.max_item_level-5,
+            name=tier.tome_gear_name,
+        )
+        crafted_gear = models.Gear.objects.get(
+            item_level=tier.max_item_level-25,
+            has_weapon=True,
+            has_armour=True,
+            has_accessories=True,
+        )
+
+        current_stuff = {
+            f'current_{slot}': crafted_gear
+            for slot in ['mainhand', 'offhand', 'head', 'body', 'hands', 'legs', 'feet', 'earrings', 'necklace', 'bracelet', 'right_ring', 'left_ring']
+        }
+
+        print('Creating BIS Lists')
+        bis21 = models.BISList.objects.create(
+            bis_mainhand=raid_weapon,
+            bis_offhand=raid_weapon,
+            bis_head=raid_gear,
+            bis_body=tome_gear,
+            bis_hands=raid_gear,
+            bis_legs=tome_gear,
+            bis_feet=raid_gear,
+            bis_earrings=tome_gear,
+            bis_necklace=raid_gear,
+            bis_bracelet=tome_gear,
+            bis_right_ring=raid_gear,
+            bis_left_ring=tome_gear,
+            job_id='PLD',
+            name='Tonk',
+            owner=char2,
+            **current_stuff,
+        )
+        bis22 = models.BISList.objects.create(
+            bis_mainhand=raid_weapon,
+            bis_offhand=raid_weapon,
+            bis_head=tome_gear,
+            bis_body=raid_gear,
+            bis_hands=tome_gear,
+            bis_legs=raid_gear,
+            bis_feet=tome_gear,
+            bis_earrings=raid_gear,
+            bis_necklace=tome_gear,
+            bis_bracelet=raid_gear,
+            bis_right_ring=tome_gear,
+            bis_left_ring=raid_gear,
+            job_id='WHM',
+            name='Healz',
+            owner=char2,
+            **current_stuff,
+        )
+        bis23 = models.BISList.objects.create(
+            bis_mainhand=raid_weapon,
+            bis_offhand=raid_weapon,
+            bis_head=raid_gear,
+            bis_body=raid_gear,
+            bis_hands=tome_gear,
+            bis_legs=tome_gear,
+            bis_feet=raid_gear,
+            bis_earrings=raid_gear,
+            bis_necklace=raid_gear,
+            bis_bracelet=tome_gear,
+            bis_right_ring=tome_gear,
+            bis_left_ring=raid_gear,
+            job_id='DRG',
+            name='Deeps',
+            owner=char2,
+            **current_stuff,
+        )
+        bis31 = models.BISList.objects.create(
+            bis_mainhand=raid_weapon,
+            bis_offhand=raid_weapon,
+            bis_head=tome_gear,
+            bis_body=tome_gear,
+            bis_hands=raid_gear,
+            bis_legs=raid_gear,
+            bis_feet=tome_gear,
+            bis_earrings=tome_gear,
+            bis_necklace=raid_gear,
+            bis_bracelet=raid_gear,
+            bis_right_ring=tome_gear,
+            bis_left_ring=raid_gear,
+            job_id='WAR',
+            name='Tonk',
+            owner=char3,
+            **current_stuff,
+        )
+        bis32 = models.BISList.objects.create(
+            bis_mainhand=raid_weapon,
+            bis_offhand=raid_weapon,
+            bis_head=raid_gear,
+            bis_body=raid_gear,
+            bis_hands=raid_gear,
+            bis_legs=tome_gear,
+            bis_feet=tome_gear,
+            bis_earrings=tome_gear,
+            bis_necklace=raid_gear,
+            bis_bracelet=raid_gear,
+            bis_right_ring=tome_gear,
+            bis_left_ring=raid_gear,
+            job_id='SGE',
+            name='Healz',
+            owner=char3,
+            **current_stuff,
+        )
+        bis33 = models.BISList.objects.create(
+            bis_mainhand=raid_weapon,
+            bis_offhand=raid_weapon,
+            bis_head=tome_gear,
+            bis_body=tome_gear,
+            bis_hands=tome_gear,
+            bis_legs=raid_gear,
+            bis_feet=raid_gear,
+            bis_earrings=raid_gear,
+            bis_necklace=tome_gear,
+            bis_bracelet=tome_gear,
+            bis_right_ring=tome_gear,
+            bis_left_ring=raid_gear,
+            job_id='PCT',
+            name='Deeps',
+            owner=char3,
+            **current_stuff,
+        )
+        bis4 = models.BISList.objects.create(
+            bis_mainhand=raid_weapon,
+            bis_offhand=raid_weapon,
+            bis_head=raid_gear,
+            bis_body=tome_gear,
+            bis_hands=raid_gear,
+            bis_legs=raid_gear,
+            bis_feet=raid_gear,
+            bis_earrings=tome_gear,
+            bis_necklace=tome_gear,
+            bis_bracelet=tome_gear,
+            bis_right_ring=tome_gear,
+            bis_left_ring=raid_gear,
+            job_id='MCH',
+            name='Deeps',
+            owner=char4,
+            **current_stuff,
+        )
+
+        print('Creating Teams')
+        team1 = models.Team.objects.create(
+            invite_code=models.Team.generate_invite_code(),
+            name='Team 1',
+            tier=tier,
+        )
+        models.TeamMember.objects.create(
+            bis_list=bis21,
+            character=char2,
+            lead=True,
+            team=team1,
+        )
+        models.TeamMember.objects.create(
+            bis_list=bis32,
+            character=char3,
+            permissions=0,
+            team=team1,
+        )
+        models.TeamMember.objects.create(
+            bis_list=bis4,
+            character=char4,
+            permissions=0,
+            team=team1,
+        )
+
+        team2 = models.Team.objects.create(
+            invite_code=models.Team.generate_invite_code(),
+            name='Team 2',
+            tier=tier,
+        )
+        models.TeamMember.objects.create(
+            bis_list=bis22,
+            character=char2,
+            permissions=0,
+            team=team2,
+        )
+        models.TeamMember.objects.create(
+            bis_list=bis33,
+            character=char3,
+            lead=True,
+            team=team2,
+        )
+        models.TeamMember.objects.create(
+            bis_list=bis4,
+            character=char4,
+            permissions=0,
+            team=team2,
+        )
+
+        team3 = models.Team.objects.create(
+            invite_code=models.Team.generate_invite_code(),
+            name='Team 3',
+            tier=tier,
+        )
+        models.TeamMember.objects.create(
+            bis_list=bis23,
+            character=char2,
+            permissions=3,
+            team=team3,
+        )
+        models.TeamMember.objects.create(
+            bis_list=bis31,
+            character=char3,
+            lead=True,
+            team=team3,
+        )
+        models.TeamMember.objects.create(
+            bis_list=bis4,
+            character=char4,
+            permissions=0,
+            team=team3,
+        )
+
+        print('Done! Visit http://localhost:8080/backend/admin to log in!')
