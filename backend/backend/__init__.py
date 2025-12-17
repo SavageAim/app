eecfbe127e8fa0a5ee26753a1a9d9a99e7d31066
freyamade <frybrdrck@gmail.com>
2025-12-17 10:36:48 +0100

    7.4 release

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -2,4 +2,4 @@
 
 from .celery import app as celery_app
 
-VERSION = '20250806'
+VERSION = '20251217'
--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -12,10 +12,21 @@
     </div>
     <div class="card-content content">
       <h2 class="has-text-primary subtitle">{{ version }}</h2>
-      <div class="divider"><i class="material-icons icon">expand_more</i> FFXIV 7.3 (again) <i class="material-icons icon">expand_more</i></div>
-      <p>Fixed the Second Alliance Raid Gear Names for automatic importing.</p>
-      <p>Didn't realise the sets had different names.</p>
-      <p>The name in the list will still be War Cloud's but it applies to all of the gear from the raid.</p>
+      <div class="divider"><i class="material-icons icon">expand_more</i> FFXIV Patch 7.4 <i class="material-icons icon">expand_more</i></div>
+      <p>A new Tier, AAC Heavyweight Tier, has been added. Please ensure to update your Team to the new Tier for everything to work smoothly!</p>
+      <p>
+        The following new gear has been added;
+        <ul>
+          <li>Courtly Lover's Weapon, Armour, and Accessories - Item Level 770</li>
+          <li>Runaway Trial Weapon - Item Level 775</li>
+          <li>Bygone Brass Weapon, Armour, and Accessories - Item Level 780</li>
+          <li>Augmented Bygone Brass Weapon, Armour, and Accessories - Item Level 790</li>
+          <li>Grand Champion Armour, and Accessories - Item Level 790</li>
+          <li>Grand Champion Weapon - Item Level 795</li>
+        </ul>
+      </p>
+      <p>The default values for the Item Level filters on the BIS pages have been updated for AAC Heavyweight Tier (770 - 795).</p>
+      <p>Best of luck to all progging the Tier when it drops in January!</p>
     </div>
   </div>
 </template>
