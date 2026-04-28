260e0498f80322a7237db6651f2cfde1b31b3621
freyamade <frybrdrck@gmail.com>
2026-04-28 14:35:49 +0200

    add older missed gear

--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -12,21 +12,19 @@
     </div>
     <div class="card-content content">
       <h2 class="has-text-primary subtitle">{{ version }}</h2>
-      <div class="divider"><i class="material-icons icon">expand_more</i> FFXIV Patch 7.4 <i class="material-icons icon">expand_more</i></div>
-      <p>A new Tier, AAC Heavyweight Tier, has been added. Please ensure to update your Team to the new Tier for everything to work smoothly!</p>
+      <div class="divider"><i class="material-icons icon">expand_more</i> FFXIV Patch 7.5 <i class="material-icons icon">expand_more</i></div>
       <p>
         The following new gear has been added;
         <ul>
-          <li>Courtly Lover's Weapon, Armour, and Accessories - Item Level 770</li>
-          <li>Runaway Trial Weapon - Item Level 775</li>
-          <li>Bygone Brass Weapon, Armour, and Accessories - Item Level 780</li>
-          <li>Augmented Bygone Brass Weapon, Armour, and Accessories - Item Level 790</li>
-          <li>Grand Champion Armour, and Accessories - Item Level 790</li>
-          <li>Grand Champion Weapon - Item Level 795</li>
+          <li>Augmented Courtly Lover's Weapon, Armour, and Accessories - Item Level 780</li>
+          <li>Runaway Trial Weapon - Item Level 785</li>
+          <li>Bygone Brass Armour - Item Level 780</li>
         </ul>
       </p>
-      <p>The default values for the Item Level filters on the BIS pages have been updated for AAC Heavyweight Tier (770 - 795).</p>
-      <p>Best of luck to all progging the Tier when it drops in January!</p>
+      <div class="divider"><i class="material-icons icon">expand_more</i> Note <i class="material-icons icon">expand_more</i></div>
+      <p>I've taken a break from FFXIV recently, and as a result the site has missed a few patches of gear and such.</p>
+      <p>I've added a system in the background to keep me updated when patches happen so even while I'm away I can update the site accordingly.</p>
+      <p>I'm very sorry for the lull in support and updates while I've been away, I hope to be back come Evercold for the new expansion!</p>
     </div>
   </div>
 </template>
