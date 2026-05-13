6dbdba3d439528f2551dfd4a9d37119cc5cd6fbb
freyamade <frybrdrck@gmail.com>
2026-05-13 22:49:38 +0200

    update to announce downtime for the site

--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -12,24 +12,12 @@
     </div>
     <div class="card-content content">
       <h2 class="has-text-primary subtitle">{{ version }}</h2>
-      <div class="divider"><i class="material-icons icon">expand_more</i> FFXIV Patch 7.5 <i class="material-icons icon">expand_more</i></div>
-      <p>
-        The following new gear has been added;
-        <ul>
-          <li>Augmented Courtly Lover's Weapon, Armour, and Accessories - Item Level 780</li>
-          <li>of Naught Trial Weapons - Item Level 785</li>
-          <li>Vana'dielian Armour - Item Level 780</li>
-        </ul>
-      </p>
-      <div class="divider"><i class="material-icons icon">expand_more</i> Potential Lodestone Import Issues <i class="material-icons icon">expand_more</i></div>
-      <p>Lodestone has changed how they handle your current equipment on the website.</p>
-      <p>I caught the issue before pushing this version, and updated the scraper accordingly, but they could still change it further.</p>
-      <p>If there are any errors importing your Gear from Lodestone, I should be notified and will look into it asap, but I'm letting you know here in advance just in case!</p>
-
-      <div class="divider"><i class="material-icons icon">expand_more</i> Note <i class="material-icons icon">expand_more</i></div>
-      <p>I've taken a break from FFXIV recently, and as a result the site has missed a few patches of gear and such.</p>
-      <p>I've added a system in the background to keep me updated when patches happen so even while I'm away I can update the site accordingly.</p>
-      <p>I'm very sorry for the lull in support and updates while I've been away, I hope to be back come Evercold for the new expansion!</p>
+      <div class="divider"><i class="material-icons icon">expand_more</i> Important Announcement <i class="material-icons icon">expand_more</i></div>
+      <p>The hosting I've been using to provide this website is sadly closing down.</p>
+      <p>This does not mean the end of the site, I will be moving the site to a different host, but sadly it won't be free like my old hosting.</p>
+      <p>There will be some downtime on the site to make sure I don't lose data in the transfer.</p>
+      <p>This downtime will begin Tuesday May 19th, and I aim to have everything back up and running by the end of May with no data loss.</p>
+      <p>Very sorry for the inconvenience but I need to do it sooner rather than later!</p>
     </div>
   </div>
 </template>
