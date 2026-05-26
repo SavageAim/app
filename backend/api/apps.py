7531dd257d8a50dcbd4aa671d62049f7effd9851
freyamade <frybrdrck@gmail.com>
2026-05-26 23:36:54 +0200

    move the import back to wsgi file after defining settings module

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     name = 'api'
     default_auto_field = 'django.db.models.AutoField'
-
-    def ready(self):
-        # Import all our cloud tasks in here so they can be discovered
-        from api.tasks import *  # noqa
