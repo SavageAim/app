a66d83819d2e05a4e46ba56a8651f9526177bad2
freyamade <frybrdrck@gmail.com>
2026-05-26 23:30:32 +0200

    migrate task imports to app.ready

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     name = 'api'
     default_auto_field = 'django.db.models.AutoField'
+
+    def ready(self):
+        # Import all our cloud tasks in here so they can be discovered
+        from api.tasks import *  # noqa
