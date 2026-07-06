68cad6575e0413df90dfdba373b86f6ea3141369
freyamade <frybrdrck@gmail.com>
2026-07-06 13:03:54 +0200

    Revert "migrated rest of tasks from celery to django_cloud_tasks"
    This reverts commit 845224b02900680ef635cc0d9efc22ab687d94d0.

--- backend/api/tasks/__init__.py
+++ backend/api/tasks/__init__.py
@@ -1,17 +1,6 @@
-from .check_game_version import CheckGameVersionTask
-from .db_cleanup import DBCleanupTask
-from .refresh_tokens import RefreshTokensTask
-from .seed import SeedTask, SeedTaskSubscriber
-from .verification_reminder import VerificationReminderTask
-from .verify_character import VerifyCharacterTask, VerifyCharacterTaskSubscriber
+from .verify_character import VerifyCharacterTask
 
 __all__ = [
-    'CheckGameVersionTask',
-    'DBCleanupTask',
-    'RefreshTokensTask',
-    'SeedTask',
-    'SeedTaskSubscriber',
-    'VerificationReminderTask',
     'VerifyCharacterTask',
     'VerifyCharacterTaskSubscriber',
 ]
