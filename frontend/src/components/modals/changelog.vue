2a6ace5f8f28420386ce460f706cfea53716185f
freyamade <frybrdrck@gmail.com>
2026-08-21 17:00:36 +0200

    update changelog for new version

--- frontend/src/components/modals/changelog.vue
+++ frontend/src/components/modals/changelog.vue
@@ -12,19 +12,12 @@
     </div>
     <div class="card-content content">
       <h2 class="has-text-primary subtitle">{{ version }}</h2>
-      <div class="divider"><i class="material-icons icon">expand_more</i> Remigration <i class="material-icons icon">expand_more</i></div>
-      <p>Turns out trying to move the system to a deployment mechanism that only spins up the server when it's needed was not cheaper.</p>
-      <p>I've moved SavageAim back to simple containers on a single VM, which not only saves money but brings back all the real-time update stuff!</p>
-      <p>Should have obeyed the timeless rule of "If it ain't broke don't fix it", but at least I have a better understanding of cloud products now as a result!</p>
-      <p>
-        <ul>
-          <li>The Patreon should now have a much easier time covering costs.</li>
-          <li>A massive thank you to everyone who has supported the project already!</li>
-          <li>If it starts to out-perform the running costs, I'll invest the extra into useful upgrades.</li>
-          <li>If you have any suggestions for these upgrades, please feel free to let me know!</li>
-        </ul>
-      </p>
-      <p>Thank you all for sticking by the project this long, hopefully it stays around for a long time to come!</p>
+      <div class="divider"><i class="material-icons icon">expand_more</i> Bugfixes <i class="material-icons icon">expand_more</i></div>
+      <p>Fixed a bug with background tasks (like character verification) that were failing to be started properly.</p>
+
+      <div class="divider"><i class="material-icons icon">expand_more</i> Random Authentication Failures <i class="material-icons icon">expand_more</i></div>
+      <p>If you can log in but are randomly getting 403 errors when trying to do things, please <b>Reset your Cookies for this site.</b></p>
+      <p>This is a leftover issue from the back-to-back migrations and only affects anyone who logged in while this site was hosted at <code>app.savageaim.com</code></p>
     </div>
   </div>
 </template>
